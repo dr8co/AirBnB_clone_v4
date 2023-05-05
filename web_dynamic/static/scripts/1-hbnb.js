d54f0a9c18a53e04c5b8f648eb7b26b743b5ed3b
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-05-05 18:11:43 +0300

    Task 2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,22 @@
+/**
+ * A script that listens for changes on each INPUT checkbox tag
+ * @type {{}}
+ */
+
+let checked = {};
+$(document).ready(function () {
+    $('input:checkbox').change(function () {
+        if ($(this).is(':checked_box')) {
+            checked[$(this).data('id')] = $(this).data('name');
+        } else {
+            delete checked[$(this).data('id')];
+        }
+        $('div.amenities h4').html(function () {
+            let amenities = [];
+            Object.keys(checked).forEach(function (key) {
+                amenities.push(checked[key]);
+            });
+            return amenities.length === 0 ? ('&nbsp') : (amenities.join(', '));
+        });
+    });
+});
